b4a911cdb21aa60d8cfe703af512515a7bc7f1a0
sloong <ifengkou@hotmail.com>
2017-05-17 22:34:10 +0800

    fix log & gitignore

--- src/main/java/cn/ifengkou/springboot/Application.java
+++ src/main/java/cn/ifengkou/springboot/Application.java
@@ -2,6 +2,7 @@ package cn.ifengkou.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Description:应用启动器
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2017/4/11 17:43
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) throws Exception {
